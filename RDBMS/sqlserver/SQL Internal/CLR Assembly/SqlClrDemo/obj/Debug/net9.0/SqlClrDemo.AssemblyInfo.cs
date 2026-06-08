86144c870d2fc5611aea3846d9b7d2a9678e0bc7
Lucas Liberatori <liberatori.lucas@gmail.com>
2026-06-08 20:59:28 -0300

    Nothing

--- RDBMS/sqlserver/SQL Internal/CLR Assembly/SqlClrDemo/obj/Debug/net9.0/SqlClrDemo.AssemblyInfo.cs
+++ RDBMS/sqlserver/SQL Internal/CLR Assembly/SqlClrDemo/obj/Debug/net9.0/SqlClrDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SqlClrDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6719d3041f69f50f778b641ec8cdbdab56f5fd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ba02e3948f7596e96c8f4069497796d144ee7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SqlClrDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SqlClrDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
